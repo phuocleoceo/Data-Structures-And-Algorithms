4bf9aad7bd71545ddcd4a26f4d3e8ee6c17ef896
phuocleoceo <ht10082001@gmail.com>
2021-07-18 11:10:27 +0700

    Permutation

---
+++ Algorithms/BackTracking/Permutation/Permutation_BT.cs
@@ -0,0 +1,50 @@
+using static System.Console;
+namespace Permutation
+{
+	public class Permutation_BT
+	{
+		private int n;
+		private int[] A;
+		private bool[] USED;
+		private static int count;
+		private const int MAX = 20;
+
+		public Permutation_BT()
+		{
+			A = new int[MAX];
+		}
+
+		public void Export()
+		{
+			Write((count++) + " : ");
+			for (int i = 1; i <= n; i++)
+			{
+				Write(A[i] + " ");
+			}
+			WriteLine("");
+		}
+
+		public void Try(int i)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				if (!USED[j])
+				{
+					A[i] = j;
+					USED[j] = true;
+					if (i == n) Export();
+					else Try(i + 1);
+					USED[j] = false;
+				}
+			}
+		}
+
+		public void Execute(int n)
+		{
+			this.n = n;
+			count = 1;
+			USED = new bool[MAX];
+			Try(1);
+		}
+	}
+}
---
+++ Algorithms/BackTracking/Permutation/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace Permutation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Hoán vị là số cách sắp xếp thứ tự của n phần tử = n!
+			// Cũng chính là chỉnh hợp không lặp n chập n
+			Permutation_BT pbt = new Permutation_BT();
+			pbt.Execute(1);
+			WriteLine("---------------------------------------");
+			pbt.Execute(2);
+			WriteLine("---------------------------------------");
+			pbt.Execute(3);
+		}
+	}
+}

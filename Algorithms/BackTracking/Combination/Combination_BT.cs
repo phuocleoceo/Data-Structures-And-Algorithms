3b0f6ac3195ed7f852597c756e9408e5726a801a
phuocleoceo <ht10082001@gmail.com>
2021-07-18 11:26:32 +0700

    Combination_BT

---
+++ Algorithms/BackTracking/Combination/Combination_BT.cs
@@ -0,0 +1,43 @@
+using static System.Console;
+namespace Combination
+{
+	public class Combination_BT
+	{
+		private int k;
+		private int n;
+		private int[] A;
+		private static int count;
+		private const int MAX = 20;
+
+		public Combination_BT() { }
+
+		public void Export()
+		{
+			Write((count++) + " : ");
+			for (int i = 1; i <= k; i++)
+			{
+				Write(A[i] + " ");
+			}
+			WriteLine("");
+		}
+
+		public void Try(int i)
+		{
+			for (int j = 1 + A[i - 1]; j <= n - k + i; j++)
+			{
+				A[i] = j;
+				if (i == k) Export();
+				else Try(i + 1);
+			}
+		}
+
+		public void Execute(int k, int n)
+		{
+			this.k = k;
+			this.n = n;
+			A = new int[MAX];
+			count = 1;
+			Try(1);
+		}
+	}
+}
---
+++ Algorithms/BackTracking/Combination/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+namespace Combination
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Cách chọn ra k phần tử trong tập hợp gồm n phần tử 
+			// không theo thứ tự được gọi là một tổ hợp chập k của n 
+			// C(k)(n)= n! / (k!(n-k)!)
+			Combination_BT cbt = new Combination_BT();
+			cbt.Execute(1, 2);
+			WriteLine("---------------------------------------");
+			cbt.Execute(2, 3);
+			WriteLine("---------------------------------------");
+			cbt.Execute(2, 5);
+		}
+	}
+}
